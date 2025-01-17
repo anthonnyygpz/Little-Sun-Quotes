d6acda17b0d20d31f41f0c7f44fc4e74ef783cd7
anthonnyygpz <anthonyygpz@gmail.com>
2025-01-17 06:05:15 -0700

    delete code unnecessary

--- copy_bnk/little_sun/components/span.py
+++
@@ -1,6 +0,0 @@
-import reflex as rx
-
-
-def create_text_span(text):
-    """Create a simple text span element."""
-    return rx.text.span(text)
--- copy_bnk/little_sun/utils/constants.py
+++
@@ -1,6 +0,0 @@
-class Colors:
-    purple = "#7C3AED"
-    purple_degraded = "#e7dbfb"
-    white = "#ffffff"
-    black = "#000000"
-    error = "red"
